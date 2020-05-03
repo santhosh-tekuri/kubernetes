0db23a99e559b5c21426ddbcc8122fd666be80cc
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2020-05-03 21:18:00 +0530

    minor changes

--- zookeeper/docker/docker-entrypoint.sh
+++ zookeeper/docker/docker-entrypoint.sh
@@ -4,9 +4,7 @@ set -e
 
 cd /apache-zookeeper-3.6.1-bin
 
-if [[ ! -f "conf/zoo.cfg" ]]; then
-    cp /config/zoo.cfg /apache-zookeeper-3.6.1-bin/conf/zoo.cfg
-fi
+cp /config/zoo.cfg /apache-zookeeper-3.6.1-bin/conf/zoo.cfg
 
 HOST=$(hostname -s)
 DOMAIN=$(hostname -d)
@@ -14,7 +12,7 @@ if [[ $HOST =~ (.*)-([0-9]+)$ ]]; then
     NAME=${BASH_REMATCH[1]}
     ORD=${BASH_REMATCH[2]}
 else
-    echo "Failed to parse ordinal from hostname $HOST"
+    echo "Failed to parse hostname $HOST"
     exit 1
 fi
 
@@ -27,8 +25,7 @@ if [[ ! -f "conf/zoo.cfg.dynamic" ]]; then
 fi
 
 if [[ ! -f "/data/myid" ]]; then
-    MY_ID=$((ORD+1))
-    echo $MY_ID > /data/myid
+    echo $((ORD+1)) > /data/myid
 fi
 
 exec "$@"
--- zookeeper/docker/zkOk.sh
+++ zookeeper/docker/zkOk.sh
@@ -4,8 +4,7 @@
 # is health. The $? variable will be set to 0 if server responds that it is 
 # healthy, or 1 if the server fails to respond.
 
-ZK_CLIENT_PORT=${ZK_CLIENT_PORT:-2181}
-OK=$(echo ruok | nc 127.0.0.1 $ZK_CLIENT_PORT)
+OK=$(echo ruok | nc 127.0.0.1 2181)
 if [ "$OK" == "imok" ]; then
     exit 0
 else
