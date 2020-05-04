1aea3f90d4d6020164040b9f74003465e902af1a
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2020-05-04 13:39:43 +0530

    add kafka files

---
+++ kafka/docker/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd /kafka_2.12-2.5.0
+
+cp /config/server.properties config/server.properties
+
+HOST=$(hostname -s)
+DOMAIN=$(hostname -d)
+if [[ $HOST =~ (.*)-([0-9]+)$ ]]; then
+    NAME=${BASH_REMATCH[1]}
+    ORD=${BASH_REMATCH[2]}
+else
+    echo "Failed to parse hostname $HOST"
+    exit 1
+fi
+
+# update broker.id value
+sed -i "s/^broker.id=.*/broker.id=$((ORD+1))/g" config/server.properties
+
+# replace ${HOSTNAME}
+sed -i "s/\${HOSTNAME}/$HOST.$DOMAIN/g" config/server.properties
+
+exec "$@"
