64e13170b6dcdf714777a8beb95f47acdcfc48b8
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2020-05-30 20:06:40 +0530

    cassandra: add optional tls support

---
+++ cassandra/tls/gen.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+cd "$(dirname "${BASH_SOURCE[0]}")"
+password=secret
+rm -f keystore truststore
+keytool -genkey -keyalg RSA -alias cassandra -keystore keystore -storepass "$password" -keypass "$password" -dname "CN=None, OU=None, O=None, L=None, C=None"
+keytool -export -alias cassandra -file cert.der -keystore keystore -storepass "$password"
+keytool -import -v -trustcacerts -alias cassandra -file cert.der -keystore truststore -storepass "$password" -noprompt
+openssl x509 -inform der -in cert.der -out cert.pem
+rm cert.der
